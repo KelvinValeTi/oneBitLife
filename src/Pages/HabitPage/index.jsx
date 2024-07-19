fdd7954a893d05f5fe8dbb19ac350e3c8a4934ca
unknown <kelvinvaleti@gmail.com>
2024-07-19 13:40:25 -0300

    notificacao

--- src/Pages/HabitPage/index.jsx
+++ src/Pages/HabitPage/index.jsx
@@ -1,14 +1,16 @@
 import { useNavigation } from "@react-navigation/native";
-import React, {useState} from "react";
+import React, {useState, useEffect, useRef} from "react";
 import {Text, View, StyleSheet, Image, TouchableOpacity, ScrollView, Alert } from "react-native";
 
 import SelectHabit from "../../Components/HabitPage/SelectHabit";
 import SelectFrequency from "../../Components/HabitPage/SelectFrequency";
+import Notification from "../../Components/HabitPage/Notification";
 
 export default function HabitPage({route}){
     const navigation = useNavigation();
     const [habitInput, setHabitInput] = useState();
     const [frequencyInput, setFrequencyInput] = useState();
+    const [notificationToggle, setNotificationToggle] = useState();
 
     const {create, habit} = route.params;
 
@@ -41,6 +43,14 @@ export default function HabitPage({route}){
                             habitFrequency={habit?.habitFrequency}
                             frequencyInput={setFrequencyInput}
                         />
+                        
+                        {frequencyInput === "Mensal" ? null :(
+                            <Notification 
+                                notificationToggle={notificationToggle}
+                                setNotificationToggle={setNotificationToggle}
+                            />
+                        )}
+
                     </View>
                 </View>
             </ScrollView>
