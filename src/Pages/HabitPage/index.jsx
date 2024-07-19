5c529af0675d762ba8299b752292b53e16197af4
unknown <kelvinvaleti@gmail.com>
2024-07-19 16:15:05 -0300

    data time picker

--- src/Pages/HabitPage/index.jsx
+++ src/Pages/HabitPage/index.jsx
@@ -5,13 +5,16 @@ import {Text, View, StyleSheet, Image, TouchableOpacity, ScrollView, Alert } fro
 import SelectHabit from "../../Components/HabitPage/SelectHabit";
 import SelectFrequency from "../../Components/HabitPage/SelectFrequency";
 import Notification from "../../Components/HabitPage/Notification";
+import TimeDatePicker from "../../Components/HabitPage/TimeDataPicker";
 
 export default function HabitPage({route}){
     const navigation = useNavigation();
     const [habitInput, setHabitInput] = useState();
     const [frequencyInput, setFrequencyInput] = useState();
     const [notificationToggle, setNotificationToggle] = useState();
-
+    const [dayNotification, setDayNotification] = useState();
+    const [timeNotification, setTimeNotification] = useState();
+    
     const {create, habit} = route.params;
 
     return(
@@ -50,6 +53,19 @@ export default function HabitPage({route}){
                                 setNotificationToggle={setNotificationToggle}
                             />
                         )}
+
+                        {notificationToggle ? (
+                            frequencyInput === "Mensal" ? null : (
+                                <TimeDatePicker
+                                    frequency={frequencyInput}
+                                    dayNotification={dayNotification}
+                                    timeNotification={timeNotification}
+                                    setDayNotification={setDayNotification}
+                                    setTimeNotification={setTimeNotification}
+                                />
+                            )
+                            ): null
+                        }
 
                     </View>
                 </View>
